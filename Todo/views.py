d9446dc2e1d53f56ed788e03de158cc2b8855e4b
tanvirkabir47 <tanvirkabir47@gmail.com>
2024-09-28 00:56:43 +0600

    search views delete

--- Todo/views.py
+++ Todo/views.py
@@ -1,14 +1,10 @@
 from rest_framework import generics
 from .serializers import TodoSerializer
 from .models import Todo
-from rest_framework.pagination import PageNumberPagination
 from django.db.models import Q
 
 # Create your views here.
 
-class TodoPagination(PageNumberPagination):
-    page_size = 10  # Set the page size here
-    page_size_query_param = 'page_size'
 
 class TodoCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
